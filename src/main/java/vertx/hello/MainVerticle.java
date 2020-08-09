b277fea23fcc171bcf90d414b7862d46ddaeb14b
sezhou <zhoushun1@gmail.com>
2020-08-09 12:45:29 -0400

    first commit

---
+++ src/main/java/vertx/hello/MainVerticle.java
@@ -0,0 +1,12 @@
+package vertx.hello;
+import io.vertx.core.AbstractVerticle;
+public class MainVerticle extends AbstractVerticle {
+
+	@Override
+	public void start() {
+    vertx.createHttpServer()
+      .requestHandler(req -> 
+       	  req.response().end("Hello Vert.x!"))
+      .listen(8080);
+    }
+}
